70d20dfa7c1f6de2729730ee11d961fd1e6313f4
Rodolpho <redsmedeiros@gmail.com>
2023-05-12 15:47:15 -0300

    primeiro

---
+++ src/main/java/com/orderService/model/Order.java
@@ -0,0 +1,31 @@
+package com.orderService.model;
+
+import java.util.List;
+
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.Table;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+@Entity
+@Table(name = "t_orders")
+@Getter
+@Setter
+@NoArgsConstructor
+@AllArgsConstructor
+public class Order {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.AUTO)
+    private Long ig;
+
+    private String orderNumber;
+
+    private List<OrderLineItems> OrderLineItemsList;
+    
+}
